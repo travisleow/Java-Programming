aee0577d267f869e5372e22924b2735100725946
Travis <travis05leow@gmail.com>
2022-10-18 13:56:26 +0800

    init

---
+++ src/Practical1/MyLoveMessage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practical1;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author travi
+ */
+public class MyLoveMessage {
+    public static void main(String[] args) {
+      JOptionPane.showMessageDialog(null, "I Love Java!", "Practical1", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
