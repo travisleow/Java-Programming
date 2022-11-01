4cc8b750170598cb1e4af86e455a809c9b452ebf
tleow <travis05leow@gmail.com>
2022-11-01 16:40:44 +0800

    qn7 edited to have methods

--- src/Practical2/Q7.java
+++ src/Practical2/Q7.java
@@ -9,11 +9,30 @@ import javax.swing.JOptionPane;
  * @author Admin
  */
 public class Q7 {
+    
     public static void main(String[] args) {
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter the Height"));
-        double width = Double.parseDouble(JOptionPane.showInputDialog("Enter the width"));
-        double area = height * width;
-        double perimeter = height + height + width + width;
-        JOptionPane.showMessageDialog(null, "Summary of Rectangle Measurements\nHeight : " + String.format("%.1f", height) + "\nWidth: " + String.format("%.1f", width) + "\nArea :" + String.format("%.1f", area) + "\nPerimeter :" + String.format("%.1f", perimeter));
+       double height = getHeight();
+       double width = getWidth();
+       double[] measurements = calculate(width, height);
+       showMsg(height, width, measurements);
+    }
+    
+    public static double getHeight() {
+         double height = Double.parseDouble(JOptionPane.showInputDialog("Enter the Height"));
+         return height;
+    }
+    
+    public static double getWidth() {
+         double width = Double.parseDouble(JOptionPane.showInputDialog("Enter the width"));
+         return width;
+    }
+    
+    public static double[] calculate(double width, double height) {
+        double[] measurements = {height*width, height + height + width + width};
+        return measurements;
+    }
+    
+    public static void showMsg(double height, double width, double[] measurements) {
+         JOptionPane.showMessageDialog(null, "Summary of Rectangle Measurements\nHeight : " + String.format("%.1f", height) + "\nWidth: " + String.format("%.1f", width) + "\nArea :" + String.format("%.1f", measurements[0]) + "\nPerimeter :" + String.format("%.1f", measurements[1]));
     }
 }
