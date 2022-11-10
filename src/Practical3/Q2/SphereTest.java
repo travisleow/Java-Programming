9366912a6f76b1386d4b613a943ce5bd8e6d35cc
Travis <travis05leow@gmail.com>
2022-11-10 15:55:54 +0800

    Prac3 Q2 fixed

--- src/Practical3/Q2/SphereTest.java
+++ src/Practical3/Q2/SphereTest.java
@@ -13,18 +13,35 @@ import javax.swing.JOptionPane;
 public class SphereTest {
 
     public static void main(String[] args) {
+        String radius = "";
         Sphere newSphere = new Sphere();
-        while (true) {
-            try {
-                double radius = Double.parseDouble(JOptionPane.showInputDialog("Enter the radius of the sphere:"));
-                newSphere.setRadius(radius);
-                JOptionPane.showMessageDialog(null, "The area of the sphere is: " + newSphere.calculateArea() + "\nThe volume of the sphere is: " + newSphere.calculateVolume(), "Sphere", JOptionPane.INFORMATION_MESSAGE);
-                break;
-
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "No value entered.\nPlease enter again.", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-
+//        while (true) {
+//            try {
+//                double radius = Double.parseDouble(JOptionPane.showInputDialog("Enter the radius of the sphere:"));
+//                newSphere.setRadius(radius);
+//                JOptionPane.showMessageDialog(null, "The area of the sphere is: " + newSphere.calculateArea() + "\nThe volume of the sphere is: " + newSphere.calculateVolume(), "Sphere", JOptionPane.INFORMATION_MESSAGE);
+//                break;
+//
+//            } catch (Exception ex) {
+//                JOptionPane.showMessageDialog(null, "No value entered.\nPlease enter again.", "Error", JOptionPane.ERROR_MESSAGE);
+//            }
+//        }
+           
+            do {
+                radius = JOptionPane.showInputDialog("Enter the radius of the sphere:");
+                if (radius == null || radius.equals("")) {
+                    JOptionPane.showMessageDialog(null, "No value entered.\nPlease enter again.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                else {
+                    try {
+                        newSphere.setRadius(Double.parseDouble(radius));
+                        JOptionPane.showMessageDialog(null, "The area of the sphere is: " + newSphere.calculateArea() + "\nThe volume of the sphere is: " + newSphere.calculateVolume(), "Sphere", JOptionPane.INFORMATION_MESSAGE);
+                        break;
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Please enter an integer.\nPlease enter again.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            } while (true);
+            
     }
 }
