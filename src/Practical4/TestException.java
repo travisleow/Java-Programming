6cb3d1ee46bd478c76e4b33ecb6f789841075ef4
Travis <travis05leow@gmail.com>
2022-11-22 13:45:15 +0800

    prac4 done

--- src/Practical4/TestException.java
+++ src/Practical4/TestException.java
@@ -1,9 +1,33 @@
-package Practical4;
 
-/**
- *
- * @author travi
- */
-public class dasd {
+import java.util.Scanner;
 
+public class TestException {
+
+    public static void main(String[] args) {
+        aMethod();
+    }
+
+    public static void aMethod() {
+        try {
+            Scanner br = new Scanner(System.in);
+            int[] numbers = {10, 20, 30};
+            System.out.print("Enter 1st Number : ");
+            int a = Integer.parseInt(br.next());
+            if (a < 0 || a > 2) {
+                throw new ArrayIndexOutOfBoundsException("1st number cannot be less than 0 and more than 2");
+            }
+            System.out.println("1");
+            System.out.print("Enter 2nd Number: ");
+            int b = Integer.parseInt(br.next());
+            System.out.println("2");
+            int c = a / b;
+            System.out.println("3");
+            System.out.println("c = " + c);
+            System.out.println(numbers[a]);
+        } catch (NumberFormatException e) {
+            System.out.println("Please input an integer.");
+        } catch (ArithmeticException e) {
+            System.out.println("Number cannot be divided by 0.");
+        }
+    }
 }
